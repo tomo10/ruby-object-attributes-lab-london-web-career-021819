d306a5724fde7dc2e93024870a8b213a1f352694
Thomas Edwards <thce28@gmail.com>
2019-02-20 09:55:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,3 +19,23 @@ class Dog
 end 
 
 class Person 
+  
+  def name=(new_name)
+    @name = new_name 
+  end 
+  
+  def name 
+    @name
+  end  
+  
+  def job=(new_job)
+    @job = new_job
+  end
+  
+  def job
+    @job
+  end 
+  
+  
+  
+end
