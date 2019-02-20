ac94f06c2466bc44a0b518dc4282c53f51fca8b8
Thomas Edwards <thce28@gmail.com>
2019-02-20 09:56:07 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,41 @@
+class Dog 
+  
+  def name=(new_name)
+    @name = new_name 
+  end 
+  
+  def name 
+    @name
+  end   
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    @breed
+  end 
+  
+end 
+
+class Person 
+  
+  def name=(new_name)
+    @name = new_name 
+  end 
+  
+  def name 
+    @name
+  end  
+  
+  def job=(new_job)
+    @job = new_job
+  end
+  
+  def job
+    @job
+  end 
+  
+  
+  
+end
